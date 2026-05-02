f6294de1d5c774f618cb53edaca063c4ebe7231c
yaramegawer <yaramegawer@users.noreply.github.com>
2026-05-02 16:22:21 +0300

    final adjustments

--- src/components/CategoryItem.tsx
+++ src/components/CategoryItem.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const CategoryItem = ({ categoryTitle, image, link } : { categoryTitle: string; image: string; link: string; }) => {
   return (
-    <div className="w-[600px] relative max-[1250px]:w-[400px] max-[1250px]:h-[400px] max-sm:w-[300px] max-sm:h-[300px]">
+    <div className="w-full relative aspect-square">
       <Link to={`/shop/${link}`}>
-      <img src={`/assets/${image}`} className="h-full w-full" />
+      <img src={`/assets/${image}`} className="h-full w-full object-cover" />
       <div className="bg-secondaryBrown text-white absolute bottom-0 w-full h-16 flex justify-center items-center max-sm:h-12">
         <h3 className="text-2xl max-sm:text-xl">{ categoryTitle }</h3>
       </div>
